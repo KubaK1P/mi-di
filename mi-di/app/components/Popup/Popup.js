2541d685c8501cc627d7f6fd9fe8c3d35c9886bc
1p22geo <1p22geodecki@gmail.com>
2025-09-04 17:23:30 +0200

    fix: fixed the popup bug

--- mi-di/app/components/Popup/Popup.js
+++ mi-di/app/components/Popup/Popup.js
@@ -1,29 +1,32 @@
 import { useState, useEffect } from "react";
-import * as Tone from "tone";
 
-export default function Popup() {
-    const [isVisible, setIsVisible] = useState(false);
+export default function Popup(key = "popupClosed") {
+  const [isVisible, setIsVisible] = useState(false);
 
-    useEffect(() => {
-        setIsVisible(true);
-    }, []);
+  useEffect(() => {
+    if (localStorage.getItem(key)) {
+      return
+    }
+    setIsVisible(true);
+  }, []);
 
-    const handleClose = () => {
-        setIsVisible(false);
-    };
+  const handleClose = () => {
+    setIsVisible(false);
+    localStorage.setItem(key, "true")
+  };
 
-    if (!isVisible) return null;
+  if (!isVisible) return null;
 
-    return (
-        <div 
-            className="fixed top-0 left-0 w-screen h-screen backdrop-blur-sm flex justify-center items-center z-1200"
-            onClick={handleClose}
-        >
-            <div className="transition-all hover:bottom-[60px] absolute cursor-pointer bottom-[40px] right-[40px] w-[20vw] h-[20vh] p-4 bg-light rounded-lg shadow-lg">
-                <p className="text-dark text-2xl font-semibold">
-                    Click this (or anything on the site) to enable audio playback for the current site.
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="fixed top-0 left-0 w-screen h-screen backdrop-blur-sm flex justify-center items-center z-1200"
+      onClick={handleClose}
+    >
+      <div className="transition-all hover:bottom-[60px] absolute cursor-pointer bottom-[40px] right-[40px] w-[20vw] h-[20vh] p-4 bg-light rounded-lg shadow-lg">
+        <p className="text-dark text-2xl font-semibold">
+          Click this (or anything on the site) to enable audio playback for the current site.
+        </p>
+      </div>
+    </div>
+  );
 }
